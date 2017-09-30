4d4e3d6f764bf749df94046b6c6413565f8ff506
anushrimarar <anushri.marar@ufl.edu>
2017-09-30 03:26:20 -0400

    Initial commit

---
+++ app/src/main/java/com/example/anush/fernweh/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.example.anush.fernweh;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by anush on 9/30/2017.
+ */
+
+public class HomeFragment extends Fragment {
+
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.home_page, container, false);
+        return  myView;
+    }
+}
