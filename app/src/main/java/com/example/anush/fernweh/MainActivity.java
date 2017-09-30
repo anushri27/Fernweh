50ad270f6a1eef302acc0a96c02f0fe26efaf6dc
anushrimarar <anushri.marar@ufl.edu>
2017-09-30 13:32:54 -0400

    shellhacks
    Commited on 9/30/17 at 1:30 PM

--- app/src/main/java/com/example/anush/fernweh/MainActivity.java
+++ app/src/main/java/com/example/anush/fernweh/MainActivity.java
@@ -27,15 +27,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -44,6 +35,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.content_frame
+                        ,new HomeFragment()).commit();
     }
 
     @Override
