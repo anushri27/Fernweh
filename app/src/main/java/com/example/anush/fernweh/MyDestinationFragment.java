0a37ee3c305a39c17aca156205eb2b2850ebb515
anushrimarar <anushri.marar@ufl.edu>
2017-09-30 21:07:22 -0400

    shellhacks
    Committed on 9/30/17 at 9:07 PM

--- app/src/main/java/com/example/anush/fernweh/MyDestinationFragment.java
+++ app/src/main/java/com/example/anush/fernweh/MyDestinationFragment.java
@@ -1,15 +1,23 @@
 package com.example.anush.fernweh;
 
+import android.Manifest;
 import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by anush on 9/30/2017.
@@ -17,22 +25,48 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 
 public class MyDestinationFragment extends Fragment implements OnMapReadyCallback {
 
-    GoogleMap nGoogleMap;
-    MapView nMapView;
-    View nView;
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
 
     View myView;
     @Nullable
+
+    @Override
+    public void  onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.my_destinations, container, false);
         return  myView;
     }
 
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
+        mMapView = (MapView) myView.findViewById(R.id.map);
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
 
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689247, -74.044))
+                .title("Statue of Liberty"));
+
+    }
+
+
 }
